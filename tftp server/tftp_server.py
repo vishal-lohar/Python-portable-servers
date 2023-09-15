f736702e7e1c816dbd36f0ebe77c2d8a068f5f2b
Vishal Lohar <138370751+vishal-lohar@users.noreply.github.com>
2023-09-15 22:26:56 +0530

    Add files via upload

---
+++ tftp server/tftp_server.py
@@ -0,0 +1,11 @@
+#Make sure to replace '/path/to/tftp/root' with the actual path to the directory you want to serve via TFTP.
+# To create a TFTP server in Python, you need to use a library that supports TFTP :
+#pip3 install tftpy
+from tftpy import TftpServer
+
+server = TftpServer('/opt/nobody')
+
+try:
+    server.listen('0.0.0.0', 69)
+except KeyboardInterrupt:
+    pass
