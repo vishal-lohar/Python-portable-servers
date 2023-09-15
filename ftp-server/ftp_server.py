79df07c9b02d299f7438bb14484a9ccafc88aa07
Vishal Lohar <138370751+vishal-lohar@users.noreply.github.com>
2023-09-16 01:15:19 +0530

    Create ftp_server.py

---
+++ ftp-server/ftp_server.py
@@ -0,0 +1,20 @@
+# Modify User Credentials and Directory
+# To create a FTP server in Python, you need to use a library that supports TFTP :
+# pip3 install pyftpdlib
+from pyftpdlib import servers
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.authorizers import DummyAuthorizer
+
+# Define user credentials and directory
+user = "user"
+password = "12345"
+directory = "/opt/nobody"
+
+authorizer = DummyAuthorizer()
+authorizer.add_user(user, password, directory, perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = servers.FTPServer(("0.0.0.0", 21), handler)
+server.serve_forever()
